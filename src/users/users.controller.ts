a9a5622a2b719102c3506705d38ac1da94c61f2c
Daler <dsarifkulo021@gmail.com>
2024-04-12 15:40:37 +0500

    auth guard was implemented

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
+import { JwtAuthGuard } from 'src/auth/jwt-auth-guard';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles-guard';
 
 @ApiTags("Users")
 @Controller('users')
@@ -19,10 +22,12 @@ export class UsersController {
     create(@Body() userDto: CreateUserDto) {
         return this.usersService.createUser(userDto)
     }
+
     @ApiOperation({summary: "Get all users"})
     @ApiResponse({status: 200, type: [User]})
-
-    @Get() 
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Get()  
     getAll() {
         return this.usersService.getAllUsers()
     }
